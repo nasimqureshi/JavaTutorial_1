13ff6c032686c947029bab1bacaef14850ad6e80
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-06 22:51:34 -0600

    commit

--- src/Conditions.java
+++ src/Conditions.java
@@ -3,13 +3,26 @@ import  java.util.*;
 public class Conditions {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int age = sc.nextInt();
+//        int x = sc.nextInt();
+//
+//        if(x % 2 == 0){
+//            System.out.println("Even");
+//        }
+//        else {
+//            System.out.println("Odd");
+//        }
+//     ?????????////////Conditional Statemant???????????????????????????????????????????????????????????
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        if(age > 18){
-            System.out.println("Adult");
-        }
-        else {
-            System.out.println("Not adult");
+        if(a == b){
+            System.out.println("Equal");
+        }else {
+            if(a > b) {
+                System.out.println("a is grater than b");
+            }else {
+                System.out.println("a is lesser than b");
+            }
         }
     }
 
